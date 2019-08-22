af779c9482a9d86cdd80dc94d8703863cc44d729
mithwa <mikki.ppm@gmail.com>
2019-08-22 20:45:56 +0530

    get code using body WIP

--- utility/get-code.controller.js
+++ utility/get-code.controller.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const getCodeService = require('./get-code.service');
 
 // routes
-router.get('/', getCode);
+router.post('/', getCode);
 
 module.exports = router;
 
 function getCode (req, res, next) {
-  getCodeService.getCode()
+  console.log('req.body', JSON.parse(req.body));
+  
+
+  getCodeService.getCode(req.user.sub, req.body.link)
     .then(uniqCode => res.json(uniqCode))
     .catch(err => next(err));
 }
--- utility/get-code.service.js
+++ utility/get-code.service.js
@@ -1,7 +1,24 @@
+const db = require('_helpers/db');
+const User = db.User;
+
 module.exports = {
     getCode
 };
 
-async function getCode() {
-  return await Math.floor(1000 + Math.random() * 9000);
+async function getCode(userID, inpURL) {
+  const USER = await User.findById(userID);
+console.log('inpURL', inpURL);
+
+  let randNum = Math.floor(1000 + Math.random() * 9000);
+  let linkObj = {
+    [randNum] : inpURL
+  };
+  
+  USER.savedLinks.push(linkObj)
+
+  await USER.save();
+  
+  console.log('USER', USER);
+  
+  return randNum; 
 }
