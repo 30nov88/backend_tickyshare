15c0f62fbc6636d0057e74ff04c976f622f745ce
mithwa <mikki.ppm@gmail.com>
2019-08-24 16:23:57 +0530

    delete and vew complete

--- utility/get-code.controller.js
+++ utility/get-code.controller.js
@@ -3,12 +3,33 @@ const router = express.Router();
 const getCodeService = require('./get-code.service');
 
 // routes
+router.get('/view-all', viewAll);
 router.post('/link', getCode);
 router.post('/', saveLinkGetCode);
 router.get('/', getLink);
 router.get('/link', getLinkAndDelete);
+router.delete('/', deleteCode);
+router.delete('/all', deleteAll);
 
 module.exports = router;
+
+function viewAll (req, res, next) {
+  getCodeService.viewAll(req.user.sub)
+    .then(linksArray => res.json(linksArray))
+    .catch(err => next(err));
+}
+
+function deleteCode (req, res, next) {
+  getCodeService.deleteCode(req.user.sub, req.query.code)
+    .then(deleteStatus => res.json(deleteStatus))
+    .catch(err => next(err));
+}
+
+function deleteAll (req, res, next) {
+  getCodeService.deleteAll(req.user.sub)
+    .then(deleteStatus => res.json(deleteStatus))
+    .catch(err => next(err));
+}
 
 function getLinkAndDelete (req, res, next) {
   getCodeService.getLinkAndDelete(req.query.code)
--- utility/get-code.service.js
+++ utility/get-code.service.js
@@ -3,11 +3,19 @@ const User = db.User;
 const PublicUsers = db.PublicUsers;
 
 module.exports = {
+    viewAll,
     getCode,
     saveLinkGetCode,
     getLink,
     getLinkAndDelete,
+    deleteCode,
+    deleteAll
 };
+
+async function viewAll ( userID ) {
+  const user = await User.findById(userID);
+  return user.savedLinks;
+}
 
 async function getLinkAndDelete ( inpCode ) {
   let publicUser = await PublicUsers.findOne({});
@@ -19,7 +27,7 @@ async function getLinkAndDelete ( inpCode ) {
     if ( spliceIndex !== -1 ) {
       spliceValue = publicUser.savedLinks[spliceIndex][inpCode];
       publicUser.savedLinks.splice(spliceIndex, 1);
-      publicUser.totalCount = (publicUser.totalCount ? publicUser.totalCount : 0) - 1;
+      // publicUser.totalCount = (publicUser.totalCount ? publicUser.totalCount : 0) - 1;
       await publicUser.save();
     } else {
       // THROW INVALID CODE ERROR
@@ -70,26 +78,62 @@ async function getCode ( inpURL ) {
   // FIND THE LATEST UPCOMING 24 HOUR CYCLE AND ADD THAT UTC AS EXPIRY TIME
 
   publicUser.savedLinks.push(linkObj);
-  publicUser.totalCount = (publicUser.totalCount ? publicUser.totalCount : 0) + 1;
+  // publicUser.totalCount = (publicUser.totalCount ? publicUser.totalCount : 0) + 1;
   await publicUser.save();
 
   return randNum;
 }
 
 async function saveLinkGetCode ( userID, inpURL ) {
-  const USER = await User.findById(userID);
-  let randNum = Math.floor(1000 + Math.random() * 9000);
+  const user = await User.findById(userID);
+  let nextNum = user.savedLinks.length + 1001;
 
-  // INCLUDE COUNT VARIABLE AND USE THE RANDNUM TO BE COUNT VARIABLE
+// INCLUDE COUNT VARIABLE AND USE THE RANDNUM TO BE COUNT VARIABLE
 // OR
 // USE THE LENGTH + 1000 AS RANDNUM
 
-let linkObj = {
-    [randNum] : inpURL
+  let linkObj = {
+    [nextNum] : inpURL
   };
 
-  USER.savedLinks.push(linkObj);
-  await USER.save();
+  user.savedLinks.push(linkObj);
+  await user.save();
 
-  return randNum;
+  return nextNum;
+}
+
+async function deleteCode ( userID, inpCode ) {
+  const user = await User.findById(userID);
+  let delStatus = "not successful";
+
+  if ( user ) {
+    let spliceIndex = user.savedLinks.findIndex(obj => Object.keys(obj)[0] === inpCode);
+
+    if ( spliceIndex !== -1 ) {
+      user.savedLinks.splice(spliceIndex, 1);
+      await user.save();
+      delStatus = "success";
+    } else {
+      // THROW INVALID CODE ERROR
+    }
+  } else {
+    // THROW INTERNAL DATA ERROR
+  } // IF-ELSE
+
+  return delStatus;
+}
+
+async function deleteAll ( userID ) {
+  const user = await User.findById(userID);
+  let delStatus = "not successful";
+
+  if ( user ) {
+    user.savedLinks = [];
+    await user.save();
+    delStatus = "success";
+  } else {
+    // THROW INTERNAL DATA ERROR
+  } // IF-ELSE
+
+  return delStatus;
 }
